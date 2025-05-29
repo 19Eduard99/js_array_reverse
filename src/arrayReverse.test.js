dedc039c080e0b3cff8b581f41250c8dfeaa75e9
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2025-05-29 20:20:48 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -1,20 +1,29 @@
-'use strict';
+"use strict";
 
 describe(`Function 'arrayReverse':`, () => {
-  const arrayReverse = require('./arrayReverse');
+  const arrayReverse = require("./arrayReverse");
 
   it(`should be declared`, () => {
     expect(arrayReverse).toBeInstanceOf(Function);
   });
 
   it(`should return an array`, () => {
-
+    expect(arrayReverse([])).toBeInstanceOf(Array);
   });
 
-  it(`should return an empty string
-    if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+  it(`should reverse word(s) in the array, (including spaces)`, () => {
+    expect(arrayReverse(["Mate", "Academy"])).toEqual([
+      "ymed",
+      "acAetaM",
+    ]);
+
+    expect(arrayReverse(["Hel lo"])).toEqual(["ol leH"]);
   });
 
-  // write more tests here
+  it("should correctly reverse array with words of different lengths ", () => {
+    const someWords = ["I", "am", "a", "student!"];
+    const reversedWords = ["!", "tn", "e", "dutsamaI"];
+
+    expect(arrayReverse(someWords)).toEqual(reversedWords);
+  });
 });
